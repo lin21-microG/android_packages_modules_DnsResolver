fb1926d227b7e5fb58fa8ac443b0dff4b519e9cc
Tom Marshall <tdm.code@gmail.com>
2020-08-25 08:31:32 -0700

    DnsResolver: Sort and cache hosts file data for fast lookup
    The hosts file is normally searched linearly.  This is very slow when
    the file is large.  To mitigate this, read the hosts file and sort the
    entries in an in-memory cache.  When an address is requested via
    gethostbyname or getaddrinfo, binary search the cache.
    
    In case where the cache is not available, return a suitable error code
    and fall back to the existing lookup code.
    
    This has been written to behave as much like the existing lookup code as
    possible.  But note bionic and glibc differ in behavior for some corner
    cases.  Choose the most standard compliant behavior for these where
    possible.  Otherwise choose the behavior that seems most reasonable.
    
    Note: this change is the analogue of the bionic change of the same name.
    Both should be kept in sync.
    
    Change-Id: I5926493864d4b1291ae83f8b601bf5dcc54085cd

--- getaddrinfo.cpp
+++ getaddrinfo.cpp
@@ -65,6 +65,8 @@
 #include "resolv_cache.h"
 #include "resolv_private.h"
 
+#include "hosts_cache.h"
+
 #define ANY 0
 
 using android::net::Experiments;
@@ -1549,6 +1551,13 @@ static bool files_getaddrinfo(const size_t netid, const char* name, const addrin
     FILE* hostf = nullptr;
 
     cur = &sentinel;
+
+    int hc_error = hc_getaddrinfo(name, pai, &cur);
+    if (hc_error != EAI_SYSTEM) {
+        *res = sentinel.ai_next;
+        return sentinel.ai_next != NULL;
+    }
+
     _sethtent(&hostf);
     while ((p = _gethtent(&hostf, name, pai)) != nullptr) {
         cur->ai_next = p;
--- sethostent.cpp
+++ sethostent.cpp
@@ -43,6 +43,8 @@
 #include "hostent.h"
 #include "resolv_private.h"
 
+#include "hosts_cache.h"
+
 constexpr int MAXALIASES = 35;
 constexpr int MAXADDRS = 35;
 
@@ -68,6 +70,11 @@ int _hf_gethtbyname2(const char* name, int af, getnamaddr* info) {
     size_t len, num, i;
     char* aliases[MAXALIASES];
     char* addr_ptrs[MAXADDRS];
+
+    int rc = hc_gethtbyname(name, af, info);
+    if (rc != NETDB_INTERNAL) {
+        return (rc == NETDB_SUCCESS ? 0 : EAI_NODATA);
+    }
 
     // TODO: Wrap the 'hf' into a RAII class or std::shared_ptr and modify the
     // sethostent_r()/endhostent_r() to get rid of manually endhostent_r(&hf) everywhere.
